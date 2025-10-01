b7c53cd9f7f4098fcdba99e6b730de567b95d525
Sthitadhi <157204956+sthdas999@users.noreply.github.com>
2025-10-01 07:43:09 +0530

    Add files via upload

---
+++ Optimized multivariate taustar computation.R
@@ -0,0 +1,31 @@
+library(parallel)
+
+# Rank-transform columns of a data matrix
+rank_transform <- function(data) {
+  apply(data, 2, rank)
+}
+# Placeholder for kernel function a(.) for quadruples
+kernel_a <- function(x1, x2, x3, x4) {
+  # Compute sign-based concordance measure here
+  # ...
+  return(sign_value)
+}
+# Compute multivariate tau* (incomplete version with subsampling)
+compute_tau_star <- function(X, Y, num_samples = 1e5) {
+  n <- nrow(X)
+  Xr <- rank_transform(X)
+  Yr <- rank_transform(Y)
+  
+  # Sample quadruples randomly for Monte Carlo approx
+  quadruples <- replicate(num_samples, sample(n, 4))
+  
+  # Vectorized computation of kernel products
+  results <- mclapply(1:num_samples, function(i) {
+    inds <- quadruples[, i]
+    ax <- kernel_a(Xr[inds[1], ], Xr[inds[2], ], Xr[inds[3], ], Xr[inds[4], ])
+    ay <- kernel_a(Yr[inds[1], ], Yr[inds[2], ], Yr[inds[3], ], Yr[inds[4], ])
+    ax * ay
+  }, mc.cores = detectCores())
+  
+  mean(unlist(results))
+}
